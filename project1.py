70a591cfae16347e367aceceecb0666721aefbf4
Fran1211 <91357881+Fran1211@users.noreply.github.com>
2022-02-28 17:27:12 -0500

    Update project1.py

--- project1.py
+++ project1.py
@@ -5,20 +5,20 @@
 #Each dependant is $3000 deduction
 #$10,000 deduction
 
-deduction = 10000                   
-tax_rate = 0.20
-dep_deduction = 3000
+deduction = 10000                                               #standard deduction set at $10,000
+tax_rate = 0.20                                                 #Tax rate set at %20
+dep_deduction = 3000                                            #Dependent deduction set at $3000 per dependent
 
-print("Enter the gross income:")
-income = float(input())
+print("Enter the gross income:")                                #Asks user for income
+income = float(input())                                         #Stores ser input into float income
 
-print("Enter the number of dependents:")
-dependents = int(input())
+print("Enter the number of dependents:")                        #Asks user for number of dependents
+dependents = int(input())                                       #Stores user input into int dependents
 
-new_income = income - deduction - dep_deduction * dependents
-total_tax = new_income * tax_rate
+new_income = income - deduction - dep_deduction * dependents    #Calculates new taxable income based on all deductions
+total_tax = new_income * tax_rate                               #Calculates total tax rate based on new taxable income 
 
-print(f' The income tax is:  {total_tax:.2f} ')
+print(f' The income tax is:  {total_tax:.2f} ')                 #Prints total taxes
 
 
 
